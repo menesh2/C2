f52cf35c9ad8d15cd38a8a73b047ed17e3359885
ormenashe <menesh2@gmail.com>
2019-01-21 14:20:56 +0200

    start the project

---
+++ C2Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C2Test
+//
+//  Created by Or Menashe on 21/01/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
